ab59d41d44187932256410c31174bfb99eb0e549
arvydasg <azegaspa@gmail.com>
2022-11-01 06:02:24 +0200

    removed old components

--- appsas/src/index.js
+++ appsas/src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import Main from "./components/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function Page() {
   return (
     <div className="container">
-      <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
