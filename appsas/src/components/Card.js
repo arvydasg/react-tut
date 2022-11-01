9448b2f549514514efda62e337588f90467995a8
arvydasg <azegaspa@gmail.com>
2022-11-01 21:46:15 +0200

    using props

--- appsas/src/components/Card.js
+++ appsas/src/components/Card.js
@@ -1,20 +1,19 @@
 import React from "react";
-import cardImage from "../images/ruta.jpg";
 import cardStar from "../images/star.jpeg";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img src={cardImage} className="card--image"></img>
+      <img src={props.img} className="card--image"></img>
       <div className="card--stats">
         <img className="card--star" src={cardStar}></img>
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">LTU</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}.</span>
       </div>
-      <p>Life lessons with Rūta Meilutyte.</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
--- appsas/src/index.js
+++ appsas/src/index.js
@@ -11,10 +11,38 @@ function Page() {
       <Navbar />
       <Hero />
       <cards className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          img="/publicImages/ruta.jpg" /* https://www.youtube.com/watch?v=taMJct5oeoI&ab_channel=EsterlingAccime */
+          rating="5.0"
+          reviewCount={6}
+          country="LTU"
+          title="Life lessons with Rūta Meilutyte."
+          price={136}
+        />
+        <Card
+          img="/publicImages/swimmer1.jpg"
+          rating="4.0"
+          reviewCount={10}
+          country="BRA"
+          title="Life lessons with Oplia Cekauske."
+          price={504}
+        />
+        <Card
+          img="/publicImages/swimmer3.jpg"
+          rating="4.0"
+          reviewCount={50}
+          country="KOR"
+          title="Life lessons with Istempta Mergaite."
+          price={234}
+        />
+        <Card
+          img="/publicImages/swimmer2.jpg"
+          rating="4.9"
+          reviewCount={1}
+          country="AUS"
+          title="Life lessons with Tikra plaukike."
+          price={333}
+        />
       </cards>
     </div>
   );
