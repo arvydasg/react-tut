c2144ad867d93ca4c2401da900b39d862fb4de71
arvydasg <azegaspa@gmail.com>
2022-11-02 19:51:56 +0200

    changed to div so it does not submit on click

--- appsas/src/components/Meme.js
+++ appsas/src/components/Meme.js
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
     <main>
-      <form className="form">
-        <inffput type="text" className="form--input" placeholder="Top Text" />
+      <div className="form">
+        <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
         <button className="form--button">Get a new meme image 🪟</button>
-      </form>
+      </div>
     </main>
   );
 }
