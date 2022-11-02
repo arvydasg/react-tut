e812458c176b18e45d42c0f521af1c5df09904ae
arvydasg <azegaspa@gmail.com>
2022-11-02 20:02:07 +0200

    added button that geerates random url from an array

--- appsas/src/components/Meme.js
+++ appsas/src/components/Meme.js
@@ -1,10 +1,20 @@
-export default function Header() {
+import React from "react";
+import memesData from "../memesData";
+
+export default function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url; // index into memes array at the index of randomnumber 5:08
+  }
   return (
     <main>
       <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 🪟</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🪟
+        </button>
       </div>
     </main>
   );
