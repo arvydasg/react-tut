a38381da83b561531e79422ccc50a2e30017fbc4
arvydasg <azegaspa@gmail.com>
2022-11-04 18:21:37 +0200

    feat: added state, memes show up on button click

--- appsas/src/components/Meme.js
+++ appsas/src/components/Meme.js
@@ -2,10 +2,12 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url; // index into memes array at the index of randomnumber 5:08
+    setMemeImage(memesArray[randomNumber].url);
   }
   return (
     <main>
@@ -16,6 +18,7 @@ export default function Meme() {
           Get a new meme image 🪟
         </button>
       </div>
+      <img src={memeImage} className="meme--image" />
     </main>
   );
 }
